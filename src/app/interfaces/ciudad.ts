806bdba9ca284d4b015da0b4fd99b16d4690a7ae
Edgar Milá <edgarmila_10@outlook.com>
2024-09-05 21:43:18 +0200

    Modificado el sistema de conteo de tiempo para optimizarlo

--- src/app/interfaces/ciudad.ts
+++ src/app/interfaces/ciudad.ts
@@ -4,6 +4,7 @@ export interface Ciudad {
     nombre: string;
     timeZone: string;
     descripcion: string;
+    timeDate: Date;
     time?: string;
     noche?: boolean;
 }
--- src/app/world-clock/world-clock.component.ts
+++ src/app/world-clock/world-clock.component.ts
@@ -11,41 +11,47 @@ import { TimeServiceService } from '../services/time-service.service';
 })
 export class WorldClockComponent implements OnInit {
   ciudades: Ciudad[] = [
-    {nombre: 'New York', timeZone: 'America/New_York', descripcion: 'La ciudad que nunca duerme, conocida por su icónica Estatua de la Libertad y Times Square.'},
-    {nombre: 'Madrid', timeZone: 'Europe/Madrid', descripcion: 'La vibrante capital de España, famosa por su arquitectura, museos y vida nocturna.'},
-    {nombre: 'Tokyo', timeZone: 'Asia/Tokyo', descripcion: 'La capital de Japón, conocida por su tecnología avanzada y su mezcla de tradición y modernidad.'},
-    {nombre: 'London', timeZone: 'Europe/London', descripcion: 'La histórica capital del Reino Unido, famosa por el Big Ben, el Museo Británico y el London Eye.'},
-    {nombre: 'Paris', timeZone: 'Europe/Paris', descripcion: 'La ciudad del amor y la luz, famosa por la Torre Eiffel, el Louvre y su exquisita gastronomía.'},
-    {nombre: 'Berlin', timeZone: 'Europe/Berlin', descripcion: 'La capital de Alemania, conocida por su rica historia, la Puerta de Brandeburgo y su vibrante escena cultural.'},
-    {nombre: 'Sydney', timeZone: 'Australia/Sydney', descripcion: 'La icónica ciudad australiana, famosa por su Ópera, el Harbour Bridge y sus hermosas playas.'},
-    {nombre: 'Seoul', timeZone: 'Asia/Seoul', descripcion: 'La capital de Corea del Sur, conocida por su tecnología avanzada, palacios históricos y cultura pop.'},
-    {nombre: 'Mexico City', timeZone: 'America/Mexico_City', descripcion: 'La vibrante capital de México, rica en historia, cultura y una mezcla de arquitectura antigua y moderna.'},
-    {nombre: 'Sao Paulo', timeZone: 'America/Sao_Paulo', descripcion: 'La ciudad más grande de Brasil, conocida por su diversidad cultural, gastronomía y vida nocturna.'},
-    {nombre: 'Mumbai', timeZone: 'Asia/Kolkata', descripcion: 'La capital financiera de la India, famosa por Bollywood, su vibrante vida urbana y su puerto histórico.'},
-    {nombre: 'Shanghai', timeZone: 'Asia/Shanghai', descripcion: 'La metrópoli china conocida por su horizonte moderno, el Bund y su importancia en el comercio global.'},
-    {nombre: 'Bangkok', timeZone: 'Asia/Bangkok', descripcion: 'La animada capital de Tailandia, famosa por sus templos, mercados y vibrante vida nocturna.'},
-    {nombre: 'Ho Chi Minh City', timeZone: 'Asia/Ho_Chi_Minh', descripcion: 'La bulliciosa ciudad vietnamita, conocida por su historia, mercados y vibrante vida urbana.'},
-    {nombre: 'Jakarta', timeZone: 'Asia/Jakarta', descripcion: 'La capital de Indonesia, conocida por su vida vibrante, cultura diversa y crecimiento económico.'},
+    {nombre: 'New York', timeZone: 'America/New_York', descripcion: 'La ciudad que nunca duerme, conocida por su icónica Estatua de la Libertad y Times Square.', timeDate: new Date()},
+    {nombre: 'Madrid', timeZone: 'Europe/Madrid', descripcion: 'La vibrante capital de España, famosa por su arquitectura, museos y vida nocturna.', timeDate: new Date()},
+    {nombre: 'Tokyo', timeZone: 'Asia/Tokyo', descripcion: 'La capital de Japón, conocida por su tecnología avanzada y su mezcla de tradición y modernidad.', timeDate: new Date()},
+    {nombre: 'London', timeZone: 'Europe/London', descripcion: 'La histórica capital del Reino Unido, famosa por el Big Ben, el Museo Británico y el London Eye.', timeDate: new Date()},
+    {nombre: 'Paris', timeZone: 'Europe/Paris', descripcion: 'La ciudad del amor y la luz, famosa por la Torre Eiffel, el Louvre y su exquisita gastronomía.', timeDate: new Date()},
+    {nombre: 'Berlin', timeZone: 'Europe/Berlin', descripcion: 'La capital de Alemania, conocida por su rica historia, la Puerta de Brandeburgo y su vibrante escena cultural.', timeDate: new Date()},
+    {nombre: 'Sydney', timeZone: 'Australia/Sydney', descripcion: 'La icónica ciudad australiana, famosa por su Ópera, el Harbour Bridge y sus hermosas playas.', timeDate: new Date()},
+    {nombre: 'Seoul', timeZone: 'Asia/Seoul', descripcion: 'La capital de Corea del Sur, conocida por su tecnología avanzada, palacios históricos y cultura pop.', timeDate: new Date()},
+    {nombre: 'Mexico City', timeZone: 'America/Mexico_City', descripcion: 'La vibrante capital de México, rica en historia, cultura y una mezcla de arquitectura antigua y moderna.', timeDate: new Date()},
+    {nombre: 'Sao Paulo', timeZone: 'America/Sao_Paulo', descripcion: 'La ciudad más grande de Brasil, conocida por su diversidad cultural, gastronomía y vida nocturna.', timeDate: new Date()},
+    {nombre: 'Mumbai', timeZone: 'Asia/Kolkata', descripcion: 'La capital financiera de la India, famosa por Bollywood, su vibrante vida urbana y su puerto histórico.', timeDate: new Date()},
+    {nombre: 'Shanghai', timeZone: 'Asia/Shanghai', descripcion: 'La metrópoli china conocida por su horizonte moderno, el Bund y su importancia en el comercio global.', timeDate: new Date()},
+    {nombre: 'Bangkok', timeZone: 'Asia/Bangkok', descripcion: 'La animada capital de Tailandia, famosa por sus templos, mercados y vibrante vida nocturna.', timeDate: new Date()},
+    {nombre: 'Ho Chi Minh City', timeZone: 'Asia/Ho_Chi_Minh', descripcion: 'La bulliciosa ciudad vietnamita, conocida por su historia, mercados y vibrante vida urbana.', timeDate: new Date()},
+    {nombre: 'Jakarta', timeZone: 'Asia/Jakarta', descripcion: 'La capital de Indonesia, conocida por su vida vibrante, cultura diversa y crecimiento económico.', timeDate: new Date()},
 ];
 
 
   constructor(private timeService: TimeServiceService) { }
 
   ngOnInit(): void {
-    for(let ciudad of this.ciudades) {
-      this.timeService.getTime(ciudad.timeZone).subscribe((time)=>{
-        ciudad.time = new Date(time.dateTime).toTimeString().split(' ')[0];
-        ciudad.noche = new Date(time.dateTime).getHours() >= 22 || new Date(time.dateTime).getHours() <= 8;
+    // Hacer la llamada inicial a la API para obtener la hora de cada ciudad
+    for (let ciudad of this.ciudades) {
+      this.timeService.getTime(ciudad.timeZone).subscribe((time) => {
+        ciudad.timeDate = new Date(time.dateTime);
+        ciudad.time = ciudad.timeDate.toTimeString().split(' ')[0];; 
+        ciudad.noche = ciudad.timeDate.getHours() >= 22 || ciudad.timeDate.getHours() <= 8;
       });
     }
+  
+    // Usar setInterval para actualizar el tiempo localmente sin hacer nuevas llamadas a la API
     setInterval(() => {
-      for(let ciudad of this.ciudades) {
-        this.timeService.getTime(ciudad.timeZone).subscribe((time)=>{
-          ciudad.time = new Date(time.dateTime).toTimeString().split(' ')[0];
-          ciudad.noche = new Date(time.dateTime).getHours() >= 22 || new Date(time.dateTime).getHours() <= 8;
-          console.log(ciudad)
-        });
+      for (let ciudad of this.ciudades) {
+        // Sumar un segundo a la hora de cada ciudad
+        ciudad.timeDate?.setSeconds(ciudad.timeDate.getSeconds() + 1);
+  
+        // Actualizar si es de día o de noche
+        ciudad.noche = ciudad.timeDate.getHours() >= 22 || ciudad.timeDate?.getHours() <= 8;
+        ciudad.time = ciudad.timeDate.toTimeString().split(' ')[0];
       }
     }, 1000);
   }
+  
 }
